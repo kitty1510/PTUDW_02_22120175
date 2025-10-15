a9c320f54f0232e6831bdaea6706f4b40f9a6f58
kitty1510 <kietkiet1510@gmail.com>
2025-10-15 17:15:10 +0700

    d

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import ImageListPage from "./pages/ImageListPage";
 import ImageDetailPage from "./pages/ImageDetailPage";
 
@@ -7,8 +6,14 @@ const AppRouter = () => {
   return (
     <BrowserRouter basename="/PTUDW_02_22120175">
       <Routes>
+        {/* Redirect trang gốc sang /photos */}
+        <Route path="/" element={<Navigate to="/photos" replace />} />
+
         <Route path="/photos" element={<ImageListPage />} />
         <Route path="/photos/:id" element={<ImageDetailPage />} />
+
+        {/* Fallback cho bất kỳ URL lạ nào */}
+        <Route path="*" element={<Navigate to="/photos" replace />} />
       </Routes>
     </BrowserRouter>
   );
